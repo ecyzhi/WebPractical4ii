80226259082f6bfe5698578e6b9d7fc3f648b6d8
ecyzhi <yeaplz-wa16@student.tarc.edu.my>
2018-11-20 23:02:54 +0800

    Second commit

---
+++ WebPractical4ii/ViewBooks.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewBooks.aspx.cs" Inherits="WebPractical4ii.ViewBooks" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>View Books</h1>
+        <asp:GridView ID="gvBooks" runat="server" OnSelectedIndexChanged="gvBooks_SelectedIndexChanged" SelectMethod="gvBooks_GetData"></asp:GridView>
+
+        <br />
+
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/Menu.aspx">BackToMenu</asp:HyperLink>
+    </form>
+</body>
+</html>
---
+++ WebPractical4ii/ViewBooks.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebPractical4ii.Models;
+
+namespace WebPractical4ii
+{
+    public partial class ViewBooks : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvBooks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // The return type can be changed to IEnumerable, however to support
+        // paging and sorting, the following parameters must be added:
+        //     int maximumRows
+        //     int startRowIndex
+        //     out int totalRowCount
+        //     string sortByExpression
+        public IQueryable gvBooks_GetData()
+        {
+            BooksInventoryEntities _db = new BooksInventoryEntities();
+            return _db.Books;
+        }
+    }
+}
